50a87202329668c976b4a0de746da801d930ccef
Mónica R <a20monicarr@gmail.com>
2022-11-25 11:02:33 +0100

    añadido archivo audio

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ function toggle(q1) {
     if (obj.style.display == "block") obj.style.display = "none";
     else obj.style.display = "block";
 }
+
+
+// Reproducir musica suspense
+const musica = document.getElementById('musica');
+
+document.addEventListener('keydown', function (reproductor) {
+    if (reproductor.keyCode == 32) {
+        
+    }
+})
