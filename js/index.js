386040a12fb5c3c2255678b30b52db0e68c69471
SharonInfante <sharonshabel@gmail.com>
2022-11-30 23:32:14 +0100

    added window modal, audio icons for play and stop

--- js/index.js
+++ js/index.js
@@ -1,99 +1,130 @@
 function toggle(q1) {
-    let obj = document.getElementById(q1);
-    if (obj.style.display == "block") obj.style.display = "none";
-    else obj.style.display = "block";
+  let obj = document.getElementById(q1);
+  if (obj.style.display == "block") obj.style.display = "none";
+  else obj.style.display = "block";
 }
 //Audio
 
-let boton = document.querySelector(".boton_logo");
+/*let boton = document.querySelector(".boton_logo")
 
-boton.addEventListener("click", () => {
-    let etiquetaAudio = document.createElement("audio")
-    etiquetaAudio.setAttribute("src", "../recursos/audio/suspense.mp3")
-    etiquetaAudio.play()
-})
+    boton.addEventListener("click", () => {
+      let etiquetaAudio = document.createElement("audio")
+      etiquetaAudio.setAttribute("src", "../recursos/audio/suspense.mp3")
+      etiquetaAudio.play()
+    })
+*/
 
-window.onload=function() {
-    document.getElementById("autoplay").play();
+window.addEventListener("load",function(){
+	document.getElementById("play").addEventListener("click",sonarAudio);
+	document.getElementById("stop").addEventListener("click",callarAudio);			
+});
+
+function sonarAudio(){
+	let sonido = document.createElement("iframe");
+	sonido.setAttribute("src","recursos/audio/suspense.mp3");
+	document.body.appendChild(sonido);
+	document.getElementById("play").removeEventListener("click",sonarPajaros);
 }
 
-// Lista de nombres 
+function callarAudio(){
+	var iframe = document.getElementsByTagName("iframe");
+
+	if (iframe.length > 0){
+		iframe[0].parentNode.removeChild(iframe[0]);
+		document.getElementById("play").addEventListener("click",sonarPajaros);
+	}
+}
+
+// Lista de nombres
 
 let nameCoders = arrayCoders();
 
 let myNodelist = document.getElementsByTagName("LI");
 
 function newCoder() {
-
-    let li = document.createElement("li");
-    let name = document.getElementById("participants").value;
-    name = minusculaMayuscula(name);
-    let x = document.createTextNode(name);
-    li.appendChild(x);
-    if (name === "") {
-        swal("Añada un nombre", "", "error",{
-            buttons: false, });
-    } else {
-        if (checkName(name) == false) {
-            li.setAttribute("id", name);
-            document.getElementById("list-name").appendChild(li);
-            nameCoders = arrayCoders();
-            console.log(nameCoders);
-        }
+  let li = document.createElement("li");
+  let name = document.getElementById("participants").value;
+  name = minusculaMayuscula(name);
+  let x = document.createTextNode(name);
+  li.appendChild(x);
+  if (name === "") {
+    swal({
+      title: "ERROR",
+      text: "AÑADA UN NOMBRE A LA LISTA",
+      icon: "error",
+      buttons: "Cerrar",
+    });
+  } else {
+    if (checkName(name) == false) {
+      li.setAttribute("id", name);
+      document.getElementById("list-name").appendChild(li);
+      nameCoders = arrayCoders();
+      console.log(nameCoders);
     }
+  }
 
-    document.getElementById("participants").value = "";
+  document.getElementById("participants").value = "";
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-        nameCoders = arrayCoders();
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+      nameCoders = arrayCoders();
     }
-
-}, false);
+  },
+  false
+);
 
 function eliminarNombre() {
-    const eliminar = document.getElementsByClassName('checked');
-    while (eliminar.length > 0) {
-        eliminar[0].parentNode.removeChild(eliminar[0]);
-    } 
+  const eliminar = document.getElementsByClassName("checked");
+  while (eliminar.length > 0) {
+    eliminar[0].parentNode.removeChild(eliminar[0]);
+  }
 
-    nameCoders = arrayCoders();
+  nameCoders = arrayCoders();
 }
 
 function arrayCoders() {
-    let list = [...document.querySelectorAll("li")]
-        .map(element => element.id);
-    return (list);
+  let list = [...document.querySelectorAll("li")].map((element) => element.id);
+  return list;
 }
 
 function checkName(name) {
-    if (nameCoders.find(item => item === name)) {
-        swal("Nombre repetido", "Añada otro", "error",{
-            buttons: false, });
-    } else {
-        return false;
-    }
+  if (nameCoders.find((item) => item === name)) {
+    swal({
+      title: "NOMBRE REPETIDO",
+      text: "AÑADA OTRO A LA LISTA",
+      icon: "error",
+      buttons: "Cerrar",
+    });
+  } else {
+    return false;
+  }
 }
 
 function minusculaMayuscula(name) {
-    name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-    return name;
-}    
+  name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  return name;
+}
 
 // recarga la pagina para limpiar todo
 function limpiar() {
-
-    document.location.reload();
+  document.location.reload();
 }
 //-------------------------
 
-function killCoder(){
-    let deadIndex = Math.floor(Math.random() * nameCoders.length);
-    const deadCoder = document.getElementById(nameCoders[deadIndex]);
-    deadCoder.remove();
-    nameCoders = arrayCoders();
-    return alert("Coder "+ nameCoders[deadIndex]+ " is Dead!");
+function killCoder() {
+  let deadIndex = Math.floor(Math.random() * nameCoders.length);
+  const deadCoder = document.getElementById(nameCoders[deadIndex]);
+  let msj = `Coder ${nameCoders[deadIndex]} is Dead!`;
+  deadCoder.remove();
+  nameCoders = arrayCoders();
+  return swal({
+    title: msj,
+    icon: "recursos/img/ducks/img-pato-parca.png",
+    buttons: "Cerrar",
+  });
 }
