52cd9e68e54305f593d5be5f95ab5e6f78c2c63c
IrisGomis <gomis.f.iris@gmai.com>
2022-12-02 09:20:07 +0100

    final

--- js/index.js
+++ js/index.js
@@ -1,64 +1,29 @@
+let array2 = [];
 function toggle(q1) {
   let obj = document.getElementById(q1);
   if (obj.style.display == "block") obj.style.display = "none";
   else obj.style.display = "block";
 }
-
-/*// POPup
-const openModal = document.querySelector('.hero');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
-
-openModal.addEventListener('click', (e) =>{
-    e.preventDefault(); // Agregar parámetro para evitar comportamiento
-                        // por defecto, esto hará que no se agregue # en el enlace
-   modal.classList.add('modal--show');
-} );
-
-closeModal.addEventListener('click', (e) =>{
-    e.preventDefault(); // Agregar parámetro para evitar comportamiento
-                        // por defecto, esto hará que no se agregue # en el enlace
-   modal.classList.remove('modal--show');
-} );*/
-
-//Audio
-
-/*let boton = document.querySelector(".boton_logo")
-
-    boton.addEventListener("click", () => {
-      let etiquetaAudio = document.createElement("audio")
-      etiquetaAudio.setAttribute("src", "../recursos/audio/suspense.mp3")
-      etiquetaAudio.play()
-    })
-*/
-
-window.addEventListener("load",function(){
-	document.getElementById("play").addEventListener("click",sonarAudio);
-	document.getElementById("stop").addEventListener("click",callarAudio);			
+window.addEventListener("load", function () {
+  document.getElementById("play").addEventListener("click", sonarAudio);
+  document.getElementById("stop").addEventListener("click", callarAudio);
 });
-
-function sonarAudio(){
-	let sonido = document.createElement("iframe");
-	sonido.setAttribute("src","recursos/audio/suspense.mp3");
-	document.body.appendChild(sonido);
-	document.getElementById("play").removeEventListener("click",sonarPajaros);
+function sonarAudio() {
+  let sonido = document.createElement("iframe");
+  sonido.setAttribute("src", "recursos/audio/suspense.mp3");
+  document.body.appendChild(sonido);
+  document.getElementById("play").removeEventListener("click", sonarPajaros);
 }
-
-function callarAudio(){
-	var iframe = document.getElementsByTagName("iframe");
-
-	if (iframe.length > 0){
-		iframe[0].parentNode.removeChild(iframe[0]);
-		document.getElementById("play").addEventListener("click",sonarPajaros);
-	}
+function callarAudio() {
+  var iframe = document.getElementsByTagName("iframe");
+  if (iframe.length > 0) {
+    iframe[0].parentNode.removeChild(iframe[0]);
+    document.getElementById("play").addEventListener("click", sonarPajaros);
+  }
 }
-
 // Lista de nombres
-
 let nameCoders = arrayCoders();
-
 let mylist = document.getElementsByTagName("LI"); // cambiado mynodelist por mylist
-
 function newCoder() {
   let li = document.createElement("li");
   let name = document.getElementById("participants").value;
@@ -80,36 +45,26 @@ function newCoder() {
       console.log(nameCoders);
     }
   }
-
   document.getElementById("participants").value = "";
 }
-
-var list = document.querySelector("ul");
-list.addEventListener(
-  "click",
-  function (ev) {
-    if (ev.target.tagName === "LI") {
-      ev.target.classList.toggle("checked");
-      nameCoders = arrayCoders();
-    }
-  },
-  false
-);
-
+let list = document.querySelector("ul");
+list.addEventListener("click", function (ev) {
+  if (ev.target.tagName === "LI") {
+    ev.target.classList.toggle("checked");
+    nameCoders = arrayCoders();
+  }
+}, false);
 function eliminarNombre() {
   const eliminar = document.getElementsByClassName("checked");
   while (eliminar.length > 0) {
     eliminar[0].parentNode.removeChild(eliminar[0]);
   }
-
   nameCoders = arrayCoders();
 }
-
 function arrayCoders() {
   let list = [...document.querySelectorAll("li")].map((element) => element.id);
   return list;
 }
-
 function checkName(name) {
   if (nameCoders.find((item) => item === name)) {
     swal({
@@ -122,27 +77,51 @@ function checkName(name) {
     return false;
   }
 }
-
 function minusculaMayuscula(name) {
   name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
   return name;
 }
-
-// recarga la pagina para limpiar todo
 function limpiar() {
   document.location.reload();
 }
 //-------------------------
-
 function killCoder() {
-  let deadIndex = Math.floor(Math.random() * nameCoders.length);
-  const deadCoder = document.getElementById(nameCoders[deadIndex]);
-  let warning = `Coder ${nameCoders[deadIndex]} is Dead!`; //cambiado nombre de variable
-  deadCoder.remove();
-  nameCoders = arrayCoders();
-  return swal({
-    title: warning, // cambiado msj a warnig
-    icon: "recursos/img/ducks/img-pato-parca.png",
-    buttons: "Cerrar",
-  });
+  let msj = "";
+  temp = 9000;
+  for (let i = 0; i < localStorage.length; i++) {
+    array2.push(localStorage.getItem(`${i}`));
+  }
+  console.log(localStorage.length);
+  console.log(array2.length);
+  localStorage.clear();
+  let deadIndex = Math.floor(Math.random() * array2.length);
+  let dead = array2[`${deadIndex}`];
+  if (array2.length == 1) {
+    temp = 0;
+    swal({
+      title: `Coder ${array2[0]} is Win!`,
+      tex: " ",
+      icon: "recursos/img/ducks/img-pato-ganador.png",
+      buttons: "Cerrar",
+    });
+    return
+  } else if (array2.length > 1) {
+    array2.splice(deadIndex, 1);
+    swal({
+      title: `Coder ${dead} is Dead!`,
+      text: " " ,
+      icon: "recursos/img/ducks/img-pato-parca.png",
+      buttons: "Cerrar",
+    });
+  }
+  setTimeout(function () {
+    killCoder();
+  }, temp);
+}
+function setlocalStorage() {
+  let coderKey = new Map();
+  for (let i = 0; i < nameCoders.length; i++) {
+    coderKey.set(i, nameCoders[i]);
+    localStorage.setItem(`${i}`, nameCoders[i]);
+  }
 }
